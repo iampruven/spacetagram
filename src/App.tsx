5d88b39c6b7ddb2327e4163e8452954507727820
Vendy Prum <vendy@thinkful.com>
2021-09-21 16:05:54 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import "./App.css";
 function App() {
+  const [nasaData, setNasaData] = useState<undefined | any[]>();
+  const [likedPosts, setLikePosts] = useState<number[]>([]);
+  const [likeStatus, setLikeStatus] = useState<boolean>(false);
+  useEffect(() => {
+    fetch(
+      `https://api.nasa.gov/planetary/apod?count=10&api_key=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setNasaData(data);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  const onLikePosts = (index: number) => {
+    let newLikePosts = [...likedPosts, index];
+    setLikePosts(newLikePosts);
+    console.log(newLikePosts);
+    if (likeStatus === true) {
+      setLikeStatus(false);
+    } else {
+      setLikeStatus(true);
+    }
+    console.log(likeStatus);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {nasaData &&
+        nasaData.map((item: any, index: number) => {
+          return <div className = "card">
+            <ul>
+              <li>
+                <img src={item.hdurl} alt={item.title} />
+              </li>
+              <li>{item.title}</li>
+              <li>{item.date}</li>
+              <li>{item.explanation}</li>
+              <button onClick={() => onLikePosts(index)}>Like</button>
+            </ul>
+          </div>
+        })}
     </div>
   );
 }

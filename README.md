d154aaad5bc353e1b0e0bce4c2549f5f88715a4d
Vendy Prum <vendy@thinkful.com>
2021-09-21 17:27:55 -0700

    deployed

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Spacetagram 
--Share images from NASA's image APIs
+- Share images from NASA's image APIs
 
-## Requirements:
+## Link to deployed website
+- https://spacetagram-woad.vercel.app
+
+## Feature:
+
 - [X] Fetch data from one of NASA’s APIs and display the resulting images 
 - [X] Display descriptive data for each image
     - [X] title
@@ -9,11 +13,21 @@
     - [X] description
 - [X] Like an image
 - [X] Unlike an image
+- [X] Heart to represent like or unlike
 
 ## Tech Stack:
+
 - React
 - TypeScript
 
+
+### Getting started
+
 ````
-make a .env with REACT_APP_API_KEY = nasa api here
+npm i
 ````
+
+#### make an .env file: 
+```
+REACT_APP_API_KEY=hiuhiuhih
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+
+function Heart({heartColor}:any) {
+  
+  return (
+    <svg className={heartColor} viewBox="0 0 32 29.6">
+      <path
+        d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
+	c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"
+      />
+    </svg>
+  );
+}
 function App() {
   const [nasaData, setNasaData] = useState<undefined | any[]>();
   const [likedPosts, setLikePosts] = useState<number[]>([]);
@@ -27,21 +39,26 @@ function App() {
 
   return (
     <div className="container">
+      <h1>spacetagram</h1>
       {nasaData &&
         nasaData.map((item: any, index: number) => {
           return (
             <div className="card">
-              <ul>
-                <li>
-                  <img src={item.hdurl} alt={item.title} />
-                </li>
-                <li>{item.title}</li>
-                <li>{item.date}</li>
-                <li>{item.explanation}</li>             
+              <div>
+                <img src={item.hdurl} alt={item.title} />
+              </div>
+              <div className="postHeading">
+                <div>{item.title}</div>
+                <div>{item.date}</div>
+              </div>
+              <div className="desc">{item.explanation}</div>
+              <div>
                 {!likedPosts.includes(index) ? (
-                  <button onClick={() => onLikePosts(index)}>Like</button>
-                ):<button onClick={() => onRemoveLike(index)}>Unlike</button>}
-              </ul>
+                  <button className="liked-btn" onClick={() => onLikePosts(index)}><Heart heartColor='heart'/></button>
+                ) : (
+                  <button className="liked-btn" onClick={() => onRemoveLike(index)}><Heart heartColor="unlike-heart"/></button>
+                )}
+              </div>
             </div>
           );
         })}
